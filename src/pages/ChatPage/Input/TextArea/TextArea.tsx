6bd861f3372dd60b91b0693fcbf74fd7a8024386
artbokov <sikren.dev@gmail.com>
2023-05-25 13:53:13 +0300

    Send on enter in TextArea

--- src/pages/ChatPage/Input/TextArea/TextArea.tsx
+++ src/pages/ChatPage/Input/TextArea/TextArea.tsx
@@ -5,14 +5,23 @@ type textAreaProps = {
 };
 
 const TextArea = ({ onSend, message, setMessage }: textAreaProps) => {
-	const isSendRequest = (e: any) => e.ctrlKey && e.key === "Enter";
+	const onKeyDown = (e: any) => {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			if (e.ctrlKey) {
+				setMessage(message + "\n");
+				return;
+			}
+			onSend();
+		}
+	};
 
 	return (
 		<textarea
 			placeholder="Введите ваш текст тут:"
 			value={message}
 			onChange={(e) => setMessage(e.target.value)}
-			onKeyDown={(e) => isSendRequest(e) && onSend()}
+			onKeyDown={onKeyDown}
 		/>
 	);
 };
