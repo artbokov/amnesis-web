764191b3f07fa2a0ec52a2a06f21104ae605cc68
artbokov <sikren.dev@gmail.com>
2023-05-25 12:40:03 +0300

    ChatPage auto scroll to bottom

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -14,21 +14,19 @@ const ChatPage = () => {
 			setOptions(
 				newMessage.options?.map((option) => [option.name, option.text])
 			);
-			setMessages((history) => [...history, newMessage]);
+			setMessages((history) => [newMessage, ...history]);
 		});
-		messagesApi.setHistoryCallback((history) => {
-			setMessages(history.map((message) => message).reverse());
-		});
+		messagesApi.setHistoryCallback((history) => setMessages(history));
 		messagesApi.requestHistory();
 	}, []);
 
 	const onMessageSend = (newMessageText: string, attachedFiles: File[]) => {
 		setMessages([
-			...messages,
 			{
 				text: newMessageText,
 				files: attachedFiles,
 			},
+			...messages,
 		]);
 		messagesApi && messagesApi.sendMessage(newMessageText, attachedFiles);
 	};
